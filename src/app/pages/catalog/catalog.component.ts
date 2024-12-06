19fff52d2344b07f263a0ebf7a6644796830f198
gui2310g <fontesguilherme57@gmail.com>
2024-12-06 00:34:22 -0300

    [add] added product model, and added product in catalog component

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon'
+import { Product } from '../../models/product';
 @Component({
   selector: 'app-catalog',
   standalone: true,
@@ -8,5 +8,30 @@ import { MatIcon } from '@angular/material/icon'
   styleUrl: './catalog.component.scss'
 })
 export class CatalogComponent {
-
+  products: Product[] = [
+    {
+      id: 1,
+      image: 'assets/1.png',
+      name: 'computador game',
+      price: 50,
+    },
+    {
+      id: 2,
+      image: 'assets/1.png',
+      name: 'Computador Game XT com suporte',
+      price: 500
+    },
+    {
+      id: 3,
+      image: 'assets/1.png',
+      name: 'Computador Game XT',
+      price: 5000
+    },
+    {
+      id: 4,
+      image: 'assets/1.png',
+      name: 'Computador Game XT com suporte a 16 gb de memoria e processador turbo plus',
+      price: 50000   
+    }
+  ]
 }
