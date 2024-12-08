b590119559a6c9127e64c70f2680688ddb838052
gui2310g <fontesguilherme57@gmail.com>
2024-12-07 22:58:11 -0300

    [add] created product details component with routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
 export const routes: Routes = [
     {
         path: 'catalog',
         component: CatalogComponent
+    },
+    {
+        path: 'product',
+        component: ProductDetailsComponent
     }
 ];
